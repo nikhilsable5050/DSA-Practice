f67583012e9331389ed0c523bbe3de94509e7221
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-17 12:56:58 +0530

    updated array

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,12 +1,11 @@
 package Arrays;
 
 public class ReverseArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        /*
         int[] arr = {10,20,30,40,50};
-
         int i =0;
         int j = arr.length-1;
-
         while(i < j){
             int temp = arr[i];
             arr[i] = arr[j];
@@ -16,6 +15,22 @@ public class ReverseArray {
         }
         for(int k=0;k<arr.length;k++){
             System.out.print(arr[k] + " ");
+        }
+
+         */
+
+        int[] arr = {1,2,3,4,5};
+        int i= 0;
+        int j = arr.length-1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for(int k=0;k<arr.length;k++ ){
+            System.out.println(arr[k]);
         }
     }
 }
