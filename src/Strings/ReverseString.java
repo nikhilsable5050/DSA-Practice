bfe64ed944c280b6a90d1ec7cadddb8e3789a1c4
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-17 11:49:02 +0530

    updated string

--- src/Strings/ReverseString.java
+++ src/Strings/ReverseString.java
@@ -3,7 +3,7 @@ package Strings;
 import static java.util.Collections.reverse;
 
 public class ReverseString {
-    public static void main (String[] args){
+    public static void main (String[] args) {
       /*
         // StringBuilder does the same work as buffer
         StringBuffer obj = new StringBuffer("Nikhil Sable");
@@ -13,11 +13,20 @@ public class ReverseString {
 
        */
 
-        String str = "Nikhil Sable";
+        /*String str = "Nikhil Sable";
         String result = "";
         for(int i=str.length()-1;i>=0;i--){
             result = result + str.charAt(i);
         }
         System.out.println(result);
+
+         */
+        String s1 = "hello";
+        String s2 = "";
+        for(int i=s1.length()-1;i >=0;i--){
+            s2 = s2 + s1.charAt(i);
+        }
+        System.out.println(s2);
+
     }
 }
