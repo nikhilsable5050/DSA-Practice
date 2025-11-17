a51eefa94dc6cba5be887ed067967715c4f44d8c
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-17 12:17:08 +0530

    updated string

--- src/Strings/StringPalindrome.java
+++ src/Strings/StringPalindrome.java
@@ -16,9 +16,10 @@ public class StringPalindrome {
             System.out.println("Not Palindrome");
         }
     }
-
      */
 
+
+       /*
         //Tow Pointer appr
         String str = "madam";
         int i = 0;
@@ -37,6 +38,43 @@ public class StringPalindrome {
             System.out.println("Palindrome");
         } else {
             System.out.println("not");
+        }
+
+        */
+
+
+        /*
+        String s1 = "madam";
+        String res = "";
+        for(int i=s1.length()-1;i>=0;i--){
+            res = res + s1.charAt(i);
+        }
+        if(s1.equals(res)){
+            System.out.println("Palindromme");
+        }
+        else {
+            System.out.println("Not");
+        }
+         */
+
+        String str = "madam";
+        int i = 0;
+        int j = str.length()-1;
+        boolean isPalindrome = true;
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                isPalindrome = false;
+                break;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
         }
     }
 }
