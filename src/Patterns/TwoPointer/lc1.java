a5a131ba8f3ba6976eb5ef588dee3a684a2c7db6
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-21 21:59:13 +0530

    Revised for contest

--- src/Patterns/TwoPointer/lc1.java
+++ src/Patterns/TwoPointer/lc1.java
@@ -43,5 +43,20 @@ import java.util.Arrays;
         }
 
     }
+//class Solution {
+//    public int[] twoSum(int[] nums, int target)
+//
+//        for (int i = 0; i < nums.length; i++) {
+//
+//        for (int j = i + 1; j < nums.length; j++) {
+//
+//            if (nums[i] + nums[j] == target) {
+//                return new int[] {i, j}; // return the indices
+//            }
+//        }
+//    }
+//        return new int[] {}; // no solution found
+//}
+//}
 
 
