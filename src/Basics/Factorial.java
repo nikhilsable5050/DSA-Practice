495d0d648e1a25d5a55231c6fe887bfe68d139d2
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-25 17:53:53 +0530

    Add factorial program using iterative method

---
+++ src/Basics/Factorial.java
@@ -0,0 +1,22 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int Fact(int n) {
+        int result = 1;
+
+        for(int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your no :");
+        int n = sc.nextInt();
+        System.out.println(Fact(n));
+    }
+}
