c509ba9ef560e2371c74e0f04f8625a7d0007a88
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-05 09:04:52 +0530

    Update factorial logic and clean up code

--- src/Basics/Factorial.java
+++ src/Basics/Factorial.java
@@ -1,7 +1,22 @@
 package Basics;
-
 import java.util.Scanner;
 
+public class Factorial {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your no:");
+        int n = sc.nextInt();
+
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
+
+
+/*
 public class Factorial {
     static int Fact(int n) {
         int result = 1;
@@ -20,3 +35,4 @@ public class Factorial {
         System.out.println(Fact(n));
     }
 }
+ */
