861d1d592b0078c7fbc1449c534466688f14ae01
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-01 17:43:39 +0530

    Update Binary Search FindKey implementation

--- src/Searching/BinarySearch/FindKey.java
+++ src/Searching/BinarySearch/FindKey.java
@@ -2,14 +2,11 @@ package Searching.BinarySearch;
 
 // Binary search implementation to find a key in sorted array
 public class FindKey {
-
     public static int binarySearch(int[] arr, int key) {
         int l = 0;
         int h = arr.length - 1;
-
         while (l <= h) {
             int mid = l + (h - l) / 2; // safe mid calculation
-
             if (arr[mid] == key) {
                 return mid; // key found
             } else if (key < arr[mid]) {
@@ -20,7 +17,6 @@ public class FindKey {
         }
         return -1; // key not found
     }
-
     public static void main(String[] args) {
         int[] arr = {3, 5, 6, 8, 12, 15, 16, 19, 21};
         int key = 13;
