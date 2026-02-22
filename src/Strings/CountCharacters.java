0e25e2b7626e44f4ca1b4294db308432a9f899f2
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-22 10:15:38 +0530

    Add program to count characters in a string using for loop

---
+++ src/Strings/CountCharacters.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class CountCharacters {
+    public static void main(String[] args){
+        String str = "Nikhil";
+        int count = 0;
+
+        for(int i=0;i<str.length();i++){
+            count++;
+        }
+        System.out.println("Total characters: " + count);
+    }
+}
