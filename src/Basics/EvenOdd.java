1847c1fd7163d717c2df6e9c0a3bb738ce724553
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-24 16:01:25 +0530

    add evenOdd problem

--- src/Basics/EvenOdd.java
+++ src/Basics/EvenOdd.java
@@ -1,4 +1,13 @@
 package Basics;
 
 public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 2;
+        if (n % 2 == 0) {
+            System.out.println("Even");
+            }
+        if (n % 2 != 0) {
+            System.out.println("Odd");
+        }
+    }
 }
