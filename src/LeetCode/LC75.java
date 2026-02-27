386335c427b2b829dae3b54927a26754f6a47adf
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-27 20:15:12 +0530

    Solved LeetCode 75 - Sort Colors using counting approach

---
+++ src/LeetCode/LC75.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+public class LC75 {
+
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+
+        // Count frequency
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                red++;
+            }
+            else if(nums[i] == 1) {
+                white++;
+            }
+            else {
+                blue++;
+            }
+        }
+
+        int index = 0;
+
+        for(int i = 0; i < red; i++) {
+            nums[index++] = 0;
+        }
+
+        for(int i = 0; i < white; i++) {
+            nums[index++] = 1;
+        }
+
+        for(int i = 0; i < blue; i++) {
+            nums[index++] = 2;
+        }
+    }
+}
