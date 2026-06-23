b0fe8e8d6303354315ecb5d9c80c9b87b57906bd
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-23 21:51:16 +0530

    Add solution for Double Rent

---
+++ src/codechef/DoubleRent.java
@@ -0,0 +1,15 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class DoubleRent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        System.out.println(x * 2);
+
+        sc.close();
+    }
+}
