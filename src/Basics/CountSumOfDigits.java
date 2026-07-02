6573b87c4e75e97051e85c6457d65b4558ff3049
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-02 17:30:38 +0530

    feat(basics): add sum of digits program

--- src/Basics/CountSumOfDigits.java
+++ src/Basics/CountSumOfDigits.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class CountSumOfDigits {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your no :");
+
+        System.out.print("Enter your number: ");
         int n = sc.nextInt();
 
-        int sum;
-        for(sum = 0; n != 0; n /= 10) {
-            int digit = n % 10;
-            sum += digit;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;   // Get last digit
+            sum = sum + digit;    // Add it to sum
+            n = n / 10;           // Remove last digit
         }
 
-        System.out.println(sum);
+        System.out.println("Sum of digits = " + sum);
     }
 }
