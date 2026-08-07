86e5f05aa563a0cf5a5acca3dde6cebd93460eb5
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-07 20:43:14 +0530

    Implement array sorting using comparison and swap

---
+++ src/Arrays/SortArray.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {9,2,12,7,5,3,0};
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        // Print the sorted array
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
