2507695c65328b2b999f26d317565a1e1eb71af8
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-08 21:37:33 +0530

    Improve second largest element solution

--- src/Arrays/SecondLargest.java
+++ src/Arrays/SecondLargest.java
@@ -1,33 +1,28 @@
 package Arrays;
 
 public class SecondLargest {
-    static int secondLargest(int[] arr) {
-        int max1 = 0;
-        int max2 = 0;
-
-        if(arr[0] > arr[1]){
-            max1 = arr[0];
-            max2 = arr[1];
-        }
-        else{
-            max1 = arr[1];
-            max2 = arr[0];
-        }
-
-        for(int i = 2; i < arr.length; i++){
-            if(arr[i] > max1){
-                max2 = max1;   // fix here
-                max1 = arr[i];
-            }
-            else if(arr[i] > max2){
-                max2 = arr[i];
-            }
-        }
-        return max2;
-    }
-
     public static void main(String[] args) {
-        int[] arr = {20,42,6,25,30,88};
-        System.out.println(secondLargest(arr));
+        int[] arr = {20, 42, 6, 25, 30, 88};
+
+        int max = arr[0];
+        int Smax = arr[1];
+
+        if (arr[0] > arr[1]) {
+            max = arr[0];
+            Smax = arr[1];
+        } else {
+            max = arr[1];
+            Smax = arr[0];
+        }
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] > max) {
+                Smax = max;
+                max = arr[i];
+            } else if (arr[i] > Smax) {
+                Smax = arr[i];
+            }
+        }
+        System.out.println("Largest: " + max);
+        System.out.println("Second Largest: " + Smax);
     }
 }
